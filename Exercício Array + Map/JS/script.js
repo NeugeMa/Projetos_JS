10c2dc18abd0f4eb7ba4439906efabb41d546bd6
NeugeMa <mariana.n.dourado@gmail.com>
2023-08-10 18:02:22 -0300

    segunda parte + comnetarios

--- Exercício Array + Map/JS/script.js
+++ Exercício Array + Map/JS/script.js
@@ -10,5 +10,16 @@ const salariosComAumento = salarios.map(salario => {
   }
 });
 
+// Utilizando o método filter() para obter salários acima de 2500
+const salariosAcima2500 = salariosComAumento.filter(salario => salario > 2500);
+
+// Utilizando o método reduce() para somar os salários acima de 2500
+const totalSalariosAcima2500 = salariosAcima2500.reduce((total, salario) => total + salario, 0);
+
+// Exibir resultados no console
+console.log("Salários com aumento:", salariosComAumento);
+console.log("Salários acima de 2500:", salariosAcima2500);
+console.log("Total dos salários acima de 2500:", totalSalariosAcima2500);
+
 
 
