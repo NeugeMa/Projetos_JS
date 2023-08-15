a72bda58e90d8e1582d8a2866bbf87eacd76db6f
NeugeMa <mariana.n.dourado@gmail.com>
2023-08-15 08:22:00 -0300

    implementação de código js

--- Exercício Array + Map/JS/script2.js
+++ Exercício Array + Map/JS/script2.js
@@ -1,76 +1,82 @@
-
+// Definindo a matriz
 const taskList = [];
 
-        function addTask() {
-            const description = document.getElementById('description').value;
-            const author = document.getElementById('author').value;
-            const department = document.getElementById('department').value;
-            const importance = parseInt(document.getElementById('importance').value);
+// Função para adicionar uma nova tarefa
+function addTask() {
+    const description = document.getElementById('description').value;
+    const author = document.getElementById('author').value;
+    const department = document.getElementById('department').value;
+    const importance = parseInt(document.getElementById('importance').value);
 
-            const task = {
-                description: description,
-                author: author,
-                department: department,
-                importance: importance,
-                value: null,
-                duration: null
-            };
+    const task = {
+        description: description,
+        author: author,
+        department: department,
+        importance: importance,
+        value: null,
+        duration: null
+    };
 
-            taskList.push(task);
-            renderTaskList();
-            clearForm();
-        }
+    taskList.push(task);
+    renderTaskList();
+    clearForm();
+}
 
-        function renderTaskList() {
-            const taskListBody = document.getElementById('taskList');
-            taskListBody.innerHTML = '';
+// Função para renderizar a lista de tarefas
+function renderTaskList() {
+    const taskListBody = document.getElementById('taskList');
+    taskListBody.innerHTML = '';
 
-            taskList.forEach((task, index) => {
-                const row = document.createElement('tr');
-                row.innerHTML = `
-                    <td>${task.description}</td>
-                    <td>${task.author}</td>
-                    <td>${task.department}</td>
-                    <td>${task.importance}</td>
-                    <td>${task.value ? task.value : ''}</td>
-                    <td>${task.duration ? task.duration : ''}</td>
-                    <td>
-                        <button onclick="addValueToTask(${index})">Valor</button>
-                        <button onclick="addDurationToTask(${index})">Duração</button>
-                        <button onclick="removeTask(${index})">Remover</button>
-                    </td>
-                `;
+    taskList.map((task, index) => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${task.description}</td>
+            <td>${task.author}</td>
+            <td>${task.department}</td>
+            <td>${task.importance}</td>
+            <td>${task.value ? task.value : ''}</td>
+            <td>${task.duration ? task.duration : ''}</td>
+            <td>
+                <button onclick="addValueToTask(${index})">Valor</button>
+                <button onclick="addDurationToTask(${index})">Duração</button>
+                <button onclick="removeTask(${index})">Remover</button>
+            </td>
+        `;
 
-                taskListBody.appendChild(row);
-            });
-        }
+        taskListBody.appendChild(row);
+    });
+}
 
-        function addValueToTask(index) {
-            const value = prompt('Informe o valor da tarefa:');
-            if (value !== null) {
-                taskList[index].value = value;
-                renderTaskList();
-            }
-        }
+// Resto do código: funções addValueToTask, addDurationToTask, removeTask, clearForm
 
-        function addDurationToTask(index) {
-            const duration = prompt('Informe a duração da tarefa:');
-            if (duration !== null) {
-                taskList[index].duration = duration;
-                renderTaskList();
-            }
-        }
-
-        function removeTask(index) {
-            taskList.splice(index, 1);
-            renderTaskList();
-        }
-
-        function clearForm() {
-            document.getElementById('description').value = '';
-            document.getElementById('author').value = '';
-            document.getElementById('department').value = '';
-            document.getElementById('importance').value = '';
-        }
-
+function addValueToTask(index) {
+    const value = prompt('Informe o valor da tarefa:');
+    if (value !== null) {
+        taskList[index].value = value;
         renderTaskList();
+    }
+}
+
+function addDurationToTask(index) {
+    const duration = prompt('Informe a duração da tarefa:');
+    if (duration !== null) {
+        taskList[index].duration = duration;
+        renderTaskList();
+    }
+}
+
+function removeTask(index) {
+    taskList.splice(index, 1);
+    renderTaskList();
+}
+
+function clearForm() {
+    document.getElementById('description').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('department').value = '';
+    document.getElementById('importance').value = '';
+}
+
+// Chamar a função para renderizar a lista de tarefas inicialmente
+renderTaskList();
+
